6857854f08bf1a550b68f09b0fe8e0cd24b93934
DalvoiD <32579355+DalvoiD@users.noreply.github.com>
2020-06-28 23:55:48 +0300

    Add intervase + inventory

---
+++ RPG/Assets/Scripts/ButtomInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtomInfo : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public bool isDown { get; private set; }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isDown = false;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isDown = true;
+    }
+
+}
---
+++ RPG/Assets/Scripts/Inventory.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    private GameObject InventoryRef;
+    public ButtomInfo buttonI;
+
+    public bool inventoryOpened = false;
+
+    void Start()
+    {
+        InventoryRef = GameObject.Find("Inventory");
+        InventoryRef.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.I) || buttonI.isDown)
+        {
+            if (!inventoryOpened)
+            {
+                OpenInvetory();
+            }
+            else
+            {
+                CloseInvetory();
+            }
+        }
+    }
+
+    public void OpenInvetory()
+    {
+        InventoryRef.SetActive(true);
+        inventoryOpened = true;
+    }
+
+    public void CloseInvetory()
+    {
+        InventoryRef.SetActive(false);
+        inventoryOpened = false;
+
+    }
+}
